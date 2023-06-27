4178d4446068750284534baf27ff1b6962ab658c
Viral Joshi <viraljoshi@Virals-MBP.ht.home>
2023-06-27 14:52:37 -0400

    First Commit-Project Created with Frontend Module

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component</h1>} />
+          <Route path="/add" element={<h1>Add Product Component</h1>} />
+          <Route path="/update" element={<h1>Update Listing Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
+}
+export default App;
